b3190c26603f982b668a34b55a07cbcd4eafbe95
jaseemakhtar <jaseemamaljeri@gmail.com>
2019-03-26 15:57:46 +0530

    add web program seven

---
+++ webPrograms/programSeven/connection.php
@@ -0,0 +1,9 @@
+<?php
+    $server = "localhost";
+    $username = "root";
+    $password = "root";
+    $conn = mysqli_connect($server, $username, $password);
+    if (!$conn) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+?>
---
+++ webPrograms/programSeven/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Search Book</title>
+    <style>
+        *{
+            font-family: Arial;
+        }
+    </style>
+</head>
+<body>
+    <h1><center>Searching for book?</center></h1>
+    <div><center><a href="add.php">Add Book</a> &nbsp;<a href="search.php">Search Book</a></center></div>
+</body>
+</html>
