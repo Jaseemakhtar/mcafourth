8ab2f47667034155bffd5d9276129793bff08819
jaseemakhtar <jaseemamaljeri@gmail.com>
2019-04-04 11:44:22 +0530

    add web program eigth bootstrap version

---
+++ webPrograms/programEight/deptView.php
@@ -0,0 +1,75 @@
+<?php
+    $dep = $_GET['dep'];
+
+    if($dep == 1)
+        $deptName = "Information Department";
+    else if($dep == 2)
+        $deptName = "Sales Department";
+    else if($dep == 3)
+        $deptName = "Operations Department";
+    else
+        $deptName = "Research & Development Department";
+
+    $employees = $projects = '';
+
+    include('connection.php');
+    $sqlP = "SELECT count(*) AS projects FROM programeight.project WHERE dep = $dep";
+    $sqlE = "SELECT count(*) AS employees FROM programeight.employee WHERE depId = $dep";
+
+    $result = mysqli_query($conn, $sqlP);
+
+    if(mysqli_num_rows($result) > 0){
+        while($row = mysqli_fetch_assoc($result)){
+            $projects = $row['projects'];
+        }
+    }
+
+    $result = mysqli_query($conn, $sqlE);
+
+    if(mysqli_num_rows($result) > 0){
+        while($row = mysqli_fetch_assoc($result)){
+            $employees = $row['employees'];
+        }
+    }
+
+    mysqli_close($conn);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?= $deptName ?></title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <style>
+        table, td, th{
+            border: 1px solid #ddd;
+        }
+    </style>
+</head>
+<body>
+    <div class="container" style="margin-top: 10px;">
+        <nav class="navbar navbar-default">
+            <div class="navbar-header">
+                <div class="navbar-brand">Boring Company Database</div>
+            </div>
+        </nav>
+        <div class="alert alert-success" role="alert"><?= $deptName ?></div>
+        <ul class="list-group">
+            <li class="list-group-item">
+                <span class="badge"><?= $projects ?></span>
+                <a href="projectView.php?id=<?= $dep ?>">Projects</a>
+            </li>
+            <li class="list-group-item">
+                <span class="badge"><?= $employees ?></span>
+                <a href="empView.php?id=<?= $dep ?>">Employees</a>
+            </li>
+        </ul>
+        <footer>
+            <h5 align="center" >Made with <span style="color: #fc3f5e; font-size: 1.3em;">&hearts;</span></h5>
+            <h5 align="center" >by <a style="text-decoration: none; color: inherit;" href="https://jaseemakhtar.github.io"   >Jaseemakhtar &copy;</a></h6>
+        </footer>
+    </div>
+</body>
+</html>
---
+++ webPrograms/programEight/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>The Boring Company</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <style>
+        .container{
+            margin-top: 10px;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <nav class="navbar navbar-default">
+            <div class="navbar-header">
+                <div class="navbar-brand">Boring Company Database</div>
+            </div>
+        </nav>
+        <div class="jumbotron" >
+            <h3>Departments</h3>
+            <ul>
+                <li><h4><a href="deptView.php?dep=1">Information Department</a></h4></li>
+                <li><h4><a href="deptView.php?dep=2">Sales Department</a></h4></li>
+                <li><h4><a href="deptView.php?dep=3">Operations Department</a></h4></li>
+                <li><h4><a href="deptView.php?dep=4">R&amp;D Department</a></h4></li>
+            </ul>
+        </div>
+        <footer>
+            <h5 align="center" >Made with <span style="color: #fc3f5e; font-size: 1.3em;">&hearts;</span></h5>
+            <h5 align="center" >by <a style="text-decoration: none; color: inherit;" href="https://jaseemakhtar.github.io"   >Jaseemakhtar &copy;</a></h6>
+        </footer>
+    </div>
+</body>
+</html>
